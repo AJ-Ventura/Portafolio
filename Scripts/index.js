8a824056861c899cff667d8f76150cef0f118dc1
AJ-Ventura <antoniomtzventura@zohomail.com>
2025-05-01 11:57:26 -0700

    Second Commit
    Carousel added and grammar correction

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,9 +1,31 @@
 $(document).ready(function(){
-    /* Typewriter effect*/
+
+    let indexslide = 1;
+    showSlides(indexslide);
+    
+    /* Slide Selection */
+
+    $(".prev").click(function(){
+        showSlides(indexslide += -1);
+        console.log(indexslide);
+    })
+    $(".next").click(function(){
+        showSlides(indexslide += 1);
+        console.log(indexslide);
+    })
+    $(".demo-slide").click(function(){
+        let n = $(this).data('id');
+        showSlides((indexslide = n));
+        console.log(n);
+    })
+
+    /* Typing effect */
+
     $("#firstA").on("animationend", () => {
         $("#firstA").addClass("hide");
         $("#secondA").addClass("show");
     })
+    
     /* Sliding effect */
     const observer = new IntersectionObserver((entries)=>{
         entries.forEach(entry => {
@@ -18,4 +40,24 @@ $(document).ready(function(){
     $(".hidden").each(function(){
         observer.observe(this);
     })
+    /* Slides */
+    function showSlides(n){
+        let i;
+        let slides = $(".slides");
+        let dots = $(".demo-slide");
+        if (n > slides.length) {
+          indexslide = 1;
+        }
+        for (i = 0; i < slides.length; i++) {
+          slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[indexslide-1].style.display = "block";
+        dots[indexslide-1].className += " active";
+        $(".caption").html(dots[indexslide-1].alt);
+        console.log(dots[indexslide-1].alt);
+    }
+
 });
